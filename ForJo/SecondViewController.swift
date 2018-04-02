5ae1d292224edada1b01deb2f35b418312f8340e
wilson <wilson@aja.com.tw>
2018-04-02 10:29:07 +0800

    Initial Commit

---
+++ ForJo/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ForJo
+//
+//  Created by wilson on 2018/4/2.
+//  Copyright © 2018年 wilson. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
